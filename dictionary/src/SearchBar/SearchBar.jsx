fd633b3cbec93a9045543ee2987fff044f8cee01
iamhaitham <iamhaitham2011@gmail.com>
2022-11-03 23:52:50 +0200

    Add search functionality to SearchBar

--- dictionary/src/SearchBar/SearchBar.jsx
+++ dictionary/src/SearchBar/SearchBar.jsx
@@ -5,8 +5,36 @@ import {
 } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 import Wrapper from '../shared/Wrapper/Wrapper';
+import { useState } from 'react';
+import axios from 'axios';
 
 const SearchBar = () => {
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const handleSearchTermChange = (e) => {
+        setSearchTerm(e.target.value);
+    }
+
+    const handleInputKeyPress = async (e) => {
+        const enterKey = 'Enter';
+        if (e.key === enterKey) {
+            e.preventDefault();
+            await handleSearchClick();
+        }
+    }
+
+    const handleSearchClick = async () => {
+        try {
+            if (!searchTerm)
+                return;
+
+            const response = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${searchTerm}`);
+            console.log(response.data[0])
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
         <Wrapper>
             <Paper 
@@ -18,12 +46,15 @@ const SearchBar = () => {
                 sx={{ ml: 1, flex: 1 }}
                 placeholder='Search'
                 inputProps={{ 'aria-label': 'search' }}
+                onChange={ (e) => handleSearchTermChange(e) }
+                onKeyPress={ async (e) => await handleInputKeyPress(e) }
                 />
                 <IconButton 
                 type='button' 
                 sx={{ p: '10px' }} 
                 aria-label='search'
                 color='primary'
+                onClick={ async () => await handleSearchClick() }
                 >
                     <SearchIcon/>
                 </IconButton>
