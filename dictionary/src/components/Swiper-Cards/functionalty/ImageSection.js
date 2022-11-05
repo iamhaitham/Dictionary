51deeffe36e24fdfb8610434a15a7a21ca75284c
anas0522 <anas-2006-@hotmail.com>
2022-11-05 13:45:18 +0200

    Finsihed remain Images

--- dictionary/src/components/Swiper-Cards/functionalty/ImageSection.js
+++ dictionary/src/components/Swiper-Cards/functionalty/ImageSection.js
@@ -1,31 +1,25 @@
 import React from 'react'
 import { ArrowForward } from '@mui/icons-material';
-import clickOnImage from './ClickOnImage';
-import { Slide } from 'react-slideshow-image';
 import images from './dataImages';
 import 'react-slideshow-image/dist/styles.css'
-import '../index.css';
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
+import "../index.css"
 const ImageSection = () => {
-    const allImage = images
     return (
         <div style={{ width: "100%", height: "100%" }}>
-
-            <Slide arrows={true} autoplay={false} pauseOnHover={true}>
-                {allImage.map((item, index) => (
-                    <div>
-                        <div className="each-slide" onClick={() => { clickOnImage(item.url, allImage) }} key={index}>
-                            <div className='ImageContainer'>
-                                <img src={item.url} className="image-slider" />
-                                <p className='textInsideImage'>{item.text}</p>
-                                <div className='button_image'>
-                                    <p className='caption'>{item.caption}</p>
-                                    <span className='ArrowForward'><ArrowForward /></span>
-                                </div>
-                            </div>
-                        </div>
+            <div style={{ justifyContent: "center", marginLeft: "50%" }}>
+            </div>
+            <div>
+                <AliceCarousel mouseTracking items={images.map((item) => <div>
+                    <img src={item.url} className="imgs" />
+                    <div className='button_image'>
+                        <p className='caption'>{item.caption}</p>
+                        <span className='ArrowForward'><ArrowForward /></span>
                     </div>
-                ))}
-            </Slide>
+                </div>)} autoWidth={true} disableButtonsControls />
+            </div>
+
         </div>
     )
 }
--- dictionary/src/components/Swiper-Cards/index.js
+++ dictionary/src/components/Swiper-Cards/index.js
@@ -1,12 +1,11 @@
 import './index.css';
 import React from 'react'
-import { Slide } from 'react-slideshow-image';
 import ImageSection from './functionalty/ImageSection';
 const Swiper_Cards = () => {
   return (
     <div className='Container'>
       <div className='SecondContainer'>
-          <ImageSection />
+        <ImageSection />
       </div>
     </div>
   );
